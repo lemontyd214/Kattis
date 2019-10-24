d2e660606e581561436eb1a49405e0fd78e9dfa1
lemontyd214 <teng.y@husky.neu.edu>
2019-10-24 09:37:18 +0200

    Update Solutions

---
+++ Heart Rate/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    double b,p;
+    for(int i=0;i<N;i++)
+    {
+        cin>>b>>p;
+
+    }
+    return 0;
+}
